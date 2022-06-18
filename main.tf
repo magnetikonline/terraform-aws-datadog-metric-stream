472707f363a4eba9c182d8e91d022e286e244c0e
Peter Mescalchin <peter@magnetikonline.com>
2022-06-18 22:11:15 +1000

    Bump `terraform` and AWS provider version to `v4.x.x` series

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">= 1.0.0"
+  required_version = ">= 1.2.0"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.45.0"
+      version = "~> 4.16.0"
     }
   }
 }
