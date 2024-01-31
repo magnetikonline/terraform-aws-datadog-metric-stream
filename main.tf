f99034e2a91c8aa37842c76ece8b3825bf94784a
Peter Mescalchin <peter@magnetikonline.com>
2024-02-01 10:14:36 +1100

    Bump AWS provider to `v5.32.0`, Terraform version to `v1.5.0`

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">= 1.5.0"
+  required_version = ">= 1.6.0"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws",
-      version = "~> 5.17.0",
+      version = "~> 5.32.0",
     }
   }
 }
